7a273dd85fbc3b7d51afe18268b09c807dba2568
Trần Vũ Lộc <32009331+winyahoo@users.noreply.github.com>
2021-05-07 10:13:05 +0700

    update cron

--- src/apis/routers/order.js
+++ src/apis/routers/order.js
@@ -3,7 +3,23 @@ const router = express.Router();
 const auth = require("../../middlewares/auth");
 const orderController = require("../controllers/order");
 const RefundOrder = require("../models/order")
+const Order  = require("../models/order") 
+const Storage = require("../models/storage")
+var cron = require('node-cron');
+var task 
+function logName(name){
+    task = cron.schedule('*/10 * * * * *', async() => {
+        const storages = await Storage.find()
+        console.log(storages)
+      },{
+        scheduled: false
+    });
 
+    task.start()
+}
+function stoplogName(){
+    task.stop()
+}
 router.get("/", auth, orderController.getAllOrder)
 
 router.get("/:id", auth, orderController.getOrderById)
@@ -22,4 +38,18 @@ router.post("/receipt/:_id", auth, orderController.createReceipt)
 
 router.post("/payment/:_id", auth, orderController.updatePayment)
 
+router.get("/cron/:name", async (req,res)=>{
+    const name = req.params.name
+
+    logName(name)
+
+    res.send(name)
+})
+router.delete("/cron/stop", async (req,res)=>{
+
+
+    stoplogName()
+
+    res.send("stop") 
+})
 module.exports = router;
